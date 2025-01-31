054e837fd9ae0492c33137c3fec8465fc127d803
jelobap <batallerjelo10@gmail.com>
2025-01-31 18:02:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Function for the "Yes" response
 function respondYes() {
     document.getElementById("responseMessage").innerHTML = "Yay! You're my Valentine! ❤️ 😊";
-    document.getElementById("videoIframe").src = "https://www.youtube.com/embed/kPa7bsKwL-c?si=K_dAYFyDebW2Kkkf"; // Set the 'Yes' video
-    document.getElementById("videoContainer").style.display = "block"; // Show the video
+    document.getElementById("videoIframe").src = "https://www.youtube.com/embed/kPa7bsKwL-c?si=A-q4t4Y8S-IuBFZf"; // Set the 'Yes' video link
+    document.getElementById("videoContainer").style.display = "block"; // Show the video container
 }
 
 // Function for the "No" response
 function respondNo() {
     document.getElementById("responseMessage").innerHTML = "Aww, maybe next time! 😢 🎤";
-    document.getElementById("videoIframe").src = "https://www.youtube.com/embed/AtPrjYp75uA?si=gvJOLMDrs947Jsae"; // Set the 'No' video
-    document.getElementById("videoContainer").style.display = "block"; // Show the video
+    document.getElementById("videoIframe").src = "https://www.youtube.com/embed/ol2-D7NpOLk?si=cZCjNnriA99xs5YX"; // Set the 'No' video link
+    document.getElementById("videoContainer").style.display = "block"; // Show the video container
 }
